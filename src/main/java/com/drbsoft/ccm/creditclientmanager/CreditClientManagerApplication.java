d9a864a7eb20dea75a3ba32cfdab8f63efbcc3ec
Daniel Barcellos <danielpoars@gmail.com>
2018-03-16 14:57:16 -0300

    initial commit

---
+++ src/main/java/com/drbsoft/ccm/creditclientmanager/CreditClientManagerApplication.java
@@ -0,0 +1,12 @@
+package com.drbsoft.ccm.creditclientmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditClientManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditClientManagerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/drbsoft/ccm/creditclientmanager/CreditClientManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.drbsoft.ccm.creditclientmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CreditClientManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
