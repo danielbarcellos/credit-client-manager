b2520f50af1e804ee6f5a319540c93e079494a5e
Daniel Barcellos <danielpoars@gmail.com>
2018-03-18 13:32:00 -0300

    removed Constants class

--- src/main/java/com/drbsoft/ccm/utils/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.drbsoft.ccm.utils;
-
-public interface Constants {
-	
-	public static final String DATE_PATTERN = "yyyy-MM-dd'T'HH:mm:ssZ";
-	public static final String DATE_PATTERN_2 = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
-	public static final String DATE_PATTERN_MINIMAL = "yyyyMMddHHmmss";
-
-}
