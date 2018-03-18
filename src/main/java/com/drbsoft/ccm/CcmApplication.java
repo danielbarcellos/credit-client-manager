68746e03b45d0903be9bb794527b2572f92b4348
Daniel Barcellos <danielpoars@gmail.com>
2018-03-18 20:45:39 -0300

    bug fix

--- src/main/java/com/drbsoft/ccm/CcmApplication.java
+++ src/main/java/com/drbsoft/ccm/CcmApplication.java
@@ -1,5 +1,6 @@
 package com.drbsoft.ccm;
 
+import java.math.BigDecimal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 import com.drbsoft.ccm.persistence.ClientRepository;
 import com.drbsoft.ccm.persistence.entity.Client;
+import com.drbsoft.ccm.persistence.entity.CreditLine;
 
 @SpringBootApplication
 @ComponentScan("com.drbsoft.ccm")
@@ -28,6 +30,18 @@ public class CcmApplication {
 		return (args)-> {
 			Client daniel = new Client("Daniel", "Rua A");
 			Client rachel = new Client("Rachel", "Rua B");
+			
+			CreditLine limit0 = new CreditLine();
+			limit0.setValue(BigDecimal.valueOf(1500.0));
+			limit0.setVenture("B");
+			
+			daniel.setLimits(limit0);
+			
+			CreditLine limit1 = new CreditLine();
+			limit1.setValue(BigDecimal.valueOf(1500.0));
+			limit1.setVenture("B");
+			
+			rachel.setLimits(limit1);
 			
 			clientRepository.save(daniel);
 			clientRepository.save(rachel);
